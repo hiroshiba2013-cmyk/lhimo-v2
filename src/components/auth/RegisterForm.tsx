5a63df5f792616f7af3b530871aad32450379ef5
hiroshiba2013-cmyk <hiroshiba2013@gmail.com>
2026-08-17 22:27:24 +0200

    Updated RegisterForm.tsx

--- src/components/auth/RegisterForm.tsx
+++ src/components/auth/RegisterForm.tsx
@@ -118,6 +118,7 @@ export function RegisterForm({ onSuccess }: { onSuccess?: () => void }) {
   const { macroCategories } = useMacroCategories();
   console.log("MACRO CATEGORIES:", macroCategories);
  const { macroCategories } = useMacroCategories();
+  
 const { microCategories: locationMicroCategories } =
   useMicroCategories(businessForm.macroCategoryId || null);
 
@@ -126,7 +127,6 @@ const { services, loading: servicesLoading } =
 
 const { specializations, loading: specializationsLoading } =
   useSpecializations(businessForm.macroCategoryId || null);
-  
   useEffect(() => {
     if (userType === 'business' && businessLocations.length === 0) {
       const defaultHours = { open: '09:00', close: '18:00', closed: false };
